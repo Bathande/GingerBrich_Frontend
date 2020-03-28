7ef15c698c1688d7f761f73bf346f2b96c5eab67
Bathande <bndaba@retrorabbit.co.za>
2020-03-28 15:09:07 +0200

    created a vue project with typescript

---
+++ gingerbrich_front/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+   {{eric}}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+  eric = "Bathande"
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
